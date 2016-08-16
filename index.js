b54cf8abe1e163408291b6284531c9e6761293f7
Gurwinder Singh <gurwindersinghnyc@gmail.com>
2016-08-16 22:14:24 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, everybody!" + specialGuest + "!";
 
 var topic = "space";
 
