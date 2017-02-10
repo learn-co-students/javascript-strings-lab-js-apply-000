33fea6ae8b0efedb2dda92af509a305d7daacfd1
Basit Owaisi <basitowaisi@gmail.com>
2017-02-10 14:02:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var greeting = "";
 
-var specialGuest = "Neil deGrasse Tyson"
-
-var greetSpecialGuest = "" + specialGuest + "!";
-
-var topic = "space";
-
-var conversation = `${topic}`;
+var basit = "basit";
+typeof basit;
