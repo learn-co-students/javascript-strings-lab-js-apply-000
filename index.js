ad7d63919e4b4a3000ae13488d799a9ffedc1a59
Savannah Lawford <savannahroselawford@gmail.com>
2017-12-07 23:22:23 +0000

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson!"
 
-var greetSpecialGuest = "" +Hello,  + "Neil deGrasse Tyson!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about space.";
+var topic = "Let's talk about";
 
-var conversation = `${topic}`;
+var conversation = `${topic} ${topic}.`;
