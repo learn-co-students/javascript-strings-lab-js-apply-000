914c5d5e71673ba591950fce7428445d07dfcc8e
Barney <balifabi@tutanota.com>
2017-12-20 09:12:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
@@ -7,3 +7,4 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+
