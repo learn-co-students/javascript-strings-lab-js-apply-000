c1b286761144d046762a1bfd1d2ddb0303ad0590
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-18 17:54:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-var greeting = "Hello, everybody!";
-
-var specialGuest = "Neil deGrasse Tyson";
-
-var greetSpecialGuest = `Hello, ${specialGuest}!`;
-
-var topic = "space";
-
-var conversation = `Let's talk about ${topic}.`;
+var greeting = "Hello, everybody!"
+var specialGuest = "Neil deGrasse Tyson"
+var greetSpecialGuest = `Hello, ${specialGuest}!`
+var topic = "space"
+var conversation = `Let's talk about ${topic}.`
