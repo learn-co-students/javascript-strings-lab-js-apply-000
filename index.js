b95f902d7d4dce2fa52d93a3386731784676f455
mir.zohaib@hotmail.com <mir.zohaib@hotmail.com>
2018-04-06 08:55:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var greeting = "";
 
-var specialGuest = "Neil deGrasse Tyson"
+var greeting = "Hello, everybody!";
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var specialGuest = "Neil deGrasse Tyson";
+
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about ${topic}.`;
