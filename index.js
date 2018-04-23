3a5d1a999443c32cc9e241d0f1c4883336507fb3
Sam <sammyliebman@gmail.com>
2018-04-23 15:43:45 +0000

    Done.

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+/* using `${xyz}` allows you to add variables into the string
+without breaking it up into multiple strings*/
 var conversation = `Let's talk about ${topic}.`;
