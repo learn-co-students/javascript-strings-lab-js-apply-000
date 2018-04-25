4c6b5b73817109c633db4da1bf6f38ca58f91bd3
Lillian Zhang <lilyzhangyun@gmail.com>
2018-04-25 18:53:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
@@ -7,3 +7,5 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+
+
