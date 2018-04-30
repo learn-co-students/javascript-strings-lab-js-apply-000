c8689ab8038bd3d7470d1ae940a101c1fa271ea8
Mahmoud Khalifa <mahmoudkhalifa@hotmail.co.uk>
2018-04-30 20:20:49 +0000

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello, " + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + "Neil deGrasse Tyson"  + "!";
 
 
 var topic = "space";
 
-var conversation = 'Let\'s talk about space ${topic}.';
+var conversation = 'Let\'s talk about space.';
 
