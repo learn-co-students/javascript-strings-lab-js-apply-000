679a438b84cef6e98976e0b489796c5d4d1ea1c5
Cheryl Kelson <cherylkelson@icloud.com>
2018-05-23 23:08:44 +0000

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
-var specialGreeting = "Hello," + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let\'s talk about ${topic}.`;
+
 
 
 
