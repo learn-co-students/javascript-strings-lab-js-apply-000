c7770161d280d97b9bbb196d9e25eb61093246b6
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-23 18:11:16 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Let\'s talk about ${topic}`;
+var conversation = `Let's talk about ${topic}.`;
 
 console.log(conversation);
