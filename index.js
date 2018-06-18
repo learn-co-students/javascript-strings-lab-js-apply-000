e2411fffb0d3ab1f140b63fef6472b9be504a058
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-06-18 20:12:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space";
+var topic = 'space';
 
-var conversation = `${topic}`;
+var sentence = 'Let\'s talk about';
+
+var conversation = `${sentence} ${topic}.`;
