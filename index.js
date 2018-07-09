69175a602c1d14a980f620c75c04694955a9780e
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-09 07:40:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var greeting = "Hello, everybody!";
+/*var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
@@ -6,4 +6,15 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = 'Let\'s talk about space.';
+var conversation = 'Let\'s talk about space.';*/
+
+let greeting = "Hello, everybody!";
+
+let specialGuest = "Neil deGrasse Tyson";
+
+let greetSpecialGuest = `Hello, ${specialGuest}!`;
+
+let topic = "space";
+
+let conversation = `Let\'s talk about ${topic}.`
+
