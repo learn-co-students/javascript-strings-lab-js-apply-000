347902d26966463b708285051913b0580c6b8b70
gmat191@gmail.com <gmat191@gmail.com>
2018-08-07 00:44:28 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space";
 
 var conversation = `Let's talk about ${topic}.`;
+//type errors are super-common, string interpolation requires an attentive eye to detail. Well it's easy to think all you need to do is add 'Let's talk about' next to ${topic} it's very easy to forget the punctuation.
