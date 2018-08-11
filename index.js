d6b6d556bcb51970e2014161233b1908aa63ac11
samantha <sstephens614@yahoo.com>
2018-08-11 01:26:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "Hello, everybody!learn save";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
