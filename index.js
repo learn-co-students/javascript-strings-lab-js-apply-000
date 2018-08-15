2507779096120dec005ca3c03d1e57dd00fcacde
unachoza <unachoza@gmail.com>
2018-08-14 22:18:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-var greeting = "";
-
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+//var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+//var conversation = `${topic}`;
+
+function greeting() {
+  console.log("Hello, everybody!")
+}
+
+function greetSpecialGuest(){
+  console.log(`Hello, ${specialGuest}!`)
+}
+function conversation(){
+  console.log(`Let's talk about ${topic}.`)
+}
