763c7422fb40ca53aebc8aa275bb08b684094484
Sarah Pai <pai.sarah89@gmail.com>
2018-08-28 13:01:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var greeting = "Hello, everybody!";
-
+ 
 var specialGuest = "Neil deGrasse Tyson"
-"Hello, " + specialGuest + "!!";
-
+ 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
-
+ 
 var topic = "space";
-
-var conversation = `Let\'s talk about ${topic}.`;
-
+ 
+var conversation = "Let's talk about " + `${topic}.`;
