16d51a9deb7fd084585d9f9ad4a494ac16ff8da4
ezirlinger <41242608+ezirlinger@users.noreply.github.com>
2018-09-06 11:22:16 -0400

    Update index.js
    Updated with // comments

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Hello, Neil deGrasse Tyson!";
 
-var greetSpecialGuest = "Hello, " + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!"; // Using concotenation to create a string for the variable greetSpecialGuest.
 
 var topic = "space";
 
-var conversation = `Let\'s talk about ${topic}.`;
+var conversation = `Let\'s talk about ${topic}.`; // pulling in the variable topic from the global scope and crearint a string through interpolation.
