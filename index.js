8a08ff073777a1860ba15ea42758409c54c01d8e
Rosen Toshev <toshev@usc.edu>
2018-09-23 21:33:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "Hello, everybody!";
+var greeting = "Hello, everybody!"; // declares a "greeting" var
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson" // declares a specialGuest var
 
-var greetSpecialGuest = "Hello, " + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!"; // Concatenates "Hello, Neil deGrasse Tyson"
 
-var topic = "space";
+var topic = "space";  
 
-var conversation = `Let's talk about ${topic}.`;
+var conversation = `Let's talk about ${topic}.`;  // String literal for "Let's talk about space"
