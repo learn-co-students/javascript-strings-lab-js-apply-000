88f03e94afc63b91f232a8c633c6fe66ce3ae78e
mjgreene2010 <mjgreene2010@gmail.com>
2018-09-24 12:26:53 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = `Hello, ` + `${specialGuest}` + `!`;
 
+var greetSpecialGuest = `Hello, ${specialGuest}!`;
+
 var topic = "space";
 
 var conversation = `Let's talk about ${topic}.`;
