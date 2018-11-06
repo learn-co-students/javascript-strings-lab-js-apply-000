508fd7d6eca4b406dbc42c3da2b04d04855f14a6
Dania Herrera <danherrera0@gmail.com>
2018-11-06 23:00:33 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = greeting + specialGuest + "!";
 
 var topic = "space";
 
