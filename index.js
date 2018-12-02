3e76b8c95d78ef0c031ae4016599c91b6a450fa6
Robert Han <robmj.han@gmail.com>
2018-12-02 19:55:50 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Hello, Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = greeting + specialGuest + "!";
 
 var topic = "Let's talk about space.";
 
