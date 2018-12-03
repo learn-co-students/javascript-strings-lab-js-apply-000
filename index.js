bc8f69e4de3e9c711e2b9fd73903bea3ff9e7fbb
lively-teacher-9822 <ccrive10@gmail.com>
2018-12-03 23:53:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var greeting = "";
 
-var specialGuest = "Neil deGrasse Tyson"
+var greeting = "Hello, everybody!";
+console.log('greeting');
 
-var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = "space";
+var specialGuest = "Neil deGrasse Tyson!";
 
-var conversation = `${topic}`;
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+
+var topic = 'space';
+
+var conversation = `Let\'s talk about ${topic}.`;
