80b6b23304c241203dcda9a78a2a148cab6b575c
vivacious-queue-3174 <caryaharper@gmail.com>
2018-12-26 18:45:36 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,13 +1,13 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
 
-it('defines `greeting`', function() {
+it('defines `greeting`', function(' Hello, everybody ') {
   expect(greeting).toEqual("Hello, everybody!")
 })
 
-it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
+it('concatenates strings to greet a special guest in `greetSpecialGuest`', function(' Hello,' + specialGuest " ! ") {
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
 })
 
-it('interpolates a string in `conversation`', function() {
+it('interpolates a string in `conversation`', function(` Let's talk about ${topic}.) {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })
