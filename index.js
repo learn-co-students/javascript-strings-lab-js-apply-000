af41af81b61b952b52957767c378d0c6222e810f
Susan Tan <susan.tan198@gmail.com>
2019-02-01 18:59:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var greeting = "";
+var greeting = "Hello, everybody!"
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let\'s talk about ${topic}.`;
+
+/*
+
+
+it('interpolates a string in `conversation`', function() {
+  expect(conversation).toEqual(`Let's talk about ${topic}.`)
+})
+*/
