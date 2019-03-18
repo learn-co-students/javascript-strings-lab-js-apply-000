9997a02be0d5aa5da0b0f7d95d31a316e715b8a7
J03Miller <48453119+J03Miller@users.noreply.github.com>
2019-03-18 15:22:32 -0400

    Update index-test.js

--- test/index-test.js
+++ test/index-test.js
@@ -11,3 +11,9 @@ it('concatenates strings to greet a special guest in `greetSpecialGuest`', funct
 it('interpolates a string in `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })
+
+var greeting = "Hello, Everybody!"
+var specialGuest = "Neil DeGrasse Tyson"
+var greetSpecialGuest = "greeting" + specialGuest + "!";
+var topic = "space"
+var conversation = '${topic}';
