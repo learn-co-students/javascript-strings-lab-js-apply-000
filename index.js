68ff4372ea88675c8ede889aa18bf607596f318b
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-03-21 16:29:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson!"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = "space";
+var topic = "let's talk about space.";
 
 var conversation = `${topic}`;
+
+// console.log(greetSpecialGuest);
