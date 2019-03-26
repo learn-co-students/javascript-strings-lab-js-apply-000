189ce1e3db35713dd032df884cfbd1d65dd5d31a
Luke Menard <lukemenard@gmail.com>
2019-03-26 03:00:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var greeting = "Hello, everybody!";
-//Defines the variable 'greeting' and sets it equal to the string 'Hello, Everybody!'
+//Defines the global variable 'greeting'
+//Assigns 'greeting' the string 'Hello, Everybody!'
 
 var specialGuest = "Neil deGrasse Tyson"
-//Defines the variable 'specialGuest' and sets it equal to the string 'Neil deGrasse Tyson'
+//Defines the global variable 'specialGuest'
+//Assigns 'specialGuest' the string 'Neil deGrasse Tyson'
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
-//Defines the variable 'greetSpecialGuest' and sets it equal to the concatanation of the string "Hello," the variable 'specialGuest' ('Neil deGrasse Tyson'), and the string "!"
-// Equals the string "Hello, Neil deGrasse Tyson!"
+//Defines the global variable 'greetSpecialGuest'
+//Assigns 'greetSpecialGuest' the concatanation of the string "Hello," the variable 'specialGuest' ('Neil deGrasse Tyson'), and the string "!"
+// The result is the string "Hello, Neil deGrasse Tyson!"
 
 var topic = "space";
-//Defines the variable 'topic' and sets it equal to the string 'space'
+//Defines the global variable 'topic'
+//Assigns 'topic' the string 'space'
 
 var conversation = `Let's talk about ${topic}.`;
-//Defines the variable 'conversation' and sets it equal to the string 'Let's talk about ${topic}.'
-//The interpolation '${topic}' references the 'topic' variable and will return the string 'space'
+//Defines the global variable 'conversation'
+//Assigns 'conversation' the string 'Let's talk about ${topic}.'
+//The interpolation '${topic}' references the 'topic' variable
+//Because the phrase is wrapped in back ticks, the interpolation will return the string 'space'
 //The final result is the string 'Let's talk about space.'
