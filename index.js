a3741fdc6d343416a316b07f57f2a7ae7087c92f
Rex Osariemen <rexosariemen@yahoo.com>
2019-04-22 23:56:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space";
+var topic = "Let's talk about space.";
+//There is an error with the tests for the var topic.
+//my input into the 'var topic', //=> "Let's talk about space."
+//It keep repeating itself in the ouptut more than once.
+//The expected keep changing.
 
 var conversation = `${topic}`;
+
+// var topic = "Let's talk about space."
+// var conversation = `${input}`;
