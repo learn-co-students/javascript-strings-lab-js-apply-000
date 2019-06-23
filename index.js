931f2842fca00c709f95b0ec3219916a8d6ebe88
concurrent-sandbox-4247 <iamrobnyce@gmail.com>
2019-06-23 19:33:00 +0000

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var greeting = "Hello, everybody!"
 
 var specialGuest = "Hello, Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + Hello, Neil deGrasse Tyson+"!";
+var greetSpecialGuest = " Hello, Neil deGrasse Tyson+"!";
 
-var topic = "Let's talk about space";
+var topic = 'Let\'s talk about space';
 
 var conversation = `${topic}`;
