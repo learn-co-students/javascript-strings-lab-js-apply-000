47cc917808c36db673fea26983a19b02d2c7cf53
wise-pool-4507 <mrd.lascarawolesi@gmail.com>
2019-06-24 23:45:34 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
+'Hello, everybody!';
+
+'${specialGuest}!', 'High';
+
+'${3 + 2}!' // 'Hello everybody, Neil deGrasse Tyson! High 5'!;
+
 var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
