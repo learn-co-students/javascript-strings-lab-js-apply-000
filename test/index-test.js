a012b5e4f12e35cdeeafd729d261425a547f13eb
Jacqueline Theiss <jackie.theiss@yahoo.com>
2019-07-26 15:27:38 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,13 +1,10 @@
-/*global concatenatedString, describe, interpolatedString, it, myString */
+var greetings ="Hello, everybody!;'
 
-it('defines `greeting`', function() {
-  expect(greeting).toEqual("Hello, everybody!")
-})
+var Special Guest = "Neil deGresse Tyson!"
 
-it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
-  expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
-})
+var greetSpecialGuest = ""+ specialGuest +"!";
 
-it('interpolates a string in `conversation`', function() {
-  expect(conversation).toEqual(`Let's talk about ${topic}.`)
-})
+var topic = "space";
+
+var conversation = '${topic}';
+
