f116f7fcfe40ea6003e6a8225284763a01693a0a
Matthew Szabo <mattmszabo@gmail.com>
2019-08-14 18:03:45 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -6,8 +6,10 @@ it('defines `greeting`', function() {
 
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
-})
+}) 
+
 
 it('interpolates a string in `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })
+
