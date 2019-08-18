b1f897dbd008bae27b250cac74dad8e10e23e592
Ken Chow <kchow.312@gmail.com>
2019-08-18 16:43:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var question = "Let's talk about "
+
+var conversation = `${question}${topic}.`;
