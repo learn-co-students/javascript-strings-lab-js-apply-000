f70024fc82be8cde34dc6fdc6858caff2a52da5d
Sam Hoidal <sjhoidal@gmail.com>
2019-10-25 20:59:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 //defined `greeting` with string's expected value from tests
 var greeting = "Hello, everybody!";
 
+
+
 var specialGuest = "Neil deGrasse Tyson";
-//concatenated to match expected value, by adding the above variable to my string.
+//concatenated to match expected value, by adding the above variable to string.
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
+
 var topic = "space";
-//interpolated to match expected value, by inserting variable into my string. Cleaner approach than lengthy concatanations when reading code. Backticks don't require me to escape "'".
+//interpolated to match expected value, by inserting variable into string. Cleaner approach than lengthy concatanations when reading code. Backticks don't require me to escape "'".
 var conversation = `Let's talk about ${topic}.`;
