86b81ff4d95a9aaebf8d45a0ed9e45669a961cec
Serkan Sahin <ssahin55@outlook.com>
2019-11-13 18:48:22 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
-kd/*global concatenatedString, describe, interpolatedString, it, myString */
+/*global concatenatedString, describe, interpolatedString, it, myString */
 
 it('defines `greeting`', function() {
   expect(greeting).toEqual("Hello, everybody!")
