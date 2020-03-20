566b75adbdea21125246205e711ed67d5c7e7c91
Brent Simon <brent.simon26@gmail.com>
2020-03-20 00:23:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var greeting = "";
+var greeting = "Hello, Everybody";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello" + specialGuest + "!";
 
-var topic = "space";
+var topic = "Let's talk about space";
 
 var conversation = `${topic}`;
+
+
+
