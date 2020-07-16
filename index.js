a170b2ba22dfc1fafadfe6cd98cce64ea02e44f0
ZHENGJIAN LIU <comezheng@gmail.com>
2020-07-16 23:10:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var talk = "Let\'s talk about ";
+
+var conversation = `${talk + topic + "."}`;
