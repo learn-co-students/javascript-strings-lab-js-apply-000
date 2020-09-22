e5da2b312b1ba771d1dae7e9af735447b1c1d930
Kalsang Agongtsang <tznklsng@yahoo.com>
2020-09-22 00:49:39 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,6 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
-function() {
-  expect(greeting).toEqual("Hello, everybody!")
-})
+it('defines `greeting`',function() {
+  expect(greeting).toEqual("Hello, everybody!")})
 
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
